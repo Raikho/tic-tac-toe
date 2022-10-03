a397c91b8dc05d20d6cc89d52a62e0c6293f4c8f
Mateen <zahidx297@yahoo.com>
2022-10-03 19:56:54 -0400

    add DOM nodes directly to 2d array

--- javascript.js
+++ javascript.js
@@ -2,22 +2,31 @@ console.log('Hello, World!');
 
 let board = (function() {
 
-
     let array = (function() {
         let full = [];
         for (let i=0; i<3; i++){
             let row = [];
             for (let j=0; j<3; j++)
-                row.push('empty');
+                row.push({value: 'empty'});
             full.push(row);
         }
+
+        let slotNodes = document.querySelectorAll('.slot');
+        for (let i=0; i<slotNodes.length; i++) {
+            let slotNode = slotNodes[i]
+            let row = slotNode.dataset.row;
+            let col = slotNode.dataset.col;
+            full[row][col].node = slotNode
+        }
+
         return full;
     })();
 
     let add = function(row, col) {
-        array[row][col] = 'added';
+        array[row][col].value = 'circle';
+        array[row][col].node.textContent = 'circle';
     }
-    
+
     return {array, add};
 })();
 
@@ -25,5 +34,6 @@ let game = (function() {
     return {};
 })();
 
+board.add(0,2);
 console.log('board:', board, board.array);
 console.log('game:', game);
