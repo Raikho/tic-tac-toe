7713a79729ff3658dd6e886d42ddc93b16c5e4e6
Mateen <zahidx297@yahoo.com>
2022-10-03 19:31:58 -0400

    add board/game module template

--- javascript.js
+++ javascript.js
@@ -1,1 +1,25 @@
 console.log('Hello, World!');
+
+let board = (function() {
+
+    let array = (function() {
+        let out = [];
+        for (let i=0; i<9; i++)
+            out.push('empty');
+        return out;
+    })();
+
+    let add = function() {
+        console.log('added');
+    }  
+
+    return {array, add};
+})();
+
+let game = (function() {
+    return {};
+})();
+
+console.log('board:', board, board.array);
+console.log('game:', game);
+board.add(1, 3);
